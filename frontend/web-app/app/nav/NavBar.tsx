5119edcd323e39a9fd4fec484410aeba67d4214d
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-23 20:15:33 +0200

    Add Auth

--- frontend/web-app/app/nav/NavBar.tsx
+++ frontend/web-app/app/nav/NavBar.tsx
@@ -1,13 +1,22 @@
+import { getCurrentUser } from "@/app/actions/authActions";
+import LoginButton from "@/app/nav/LoginButton";
 import Logo from "@/app/nav/Logo";
 import Search from "@/app/nav/Search";
+import UserActions from "@/app/nav/UserActions";
 
-export default function NavBar() {
+export default async function NavBar() {
+    const user = await getCurrentUser();
+
     return (
         <header className="sticky top-0 z-50 flex justify-between 
             bg-white p-5 items-center text-gray-800 shadow-md">
             <Logo />
             <Search />
-            <div>Login</div>
+            {user ? (
+                <UserActions user={user} />
+            ) : (
+                <LoginButton />
+            )}
         </header>
     )
 }
