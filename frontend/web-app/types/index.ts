bc82ed561ca62e7c67b027e058b49ff3f19ddc9b
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-23 11:02:02 +0200

    Create Auction Types

---
+++ frontend/web-app/types/index.ts
@@ -0,0 +1,22 @@
+export type PagedResult<T> = {
+    results: T[];
+    pageCount: number;
+    totalCount: number;
+}
+
+export type Auction = {
+    reservePrice?: number
+    seller: string
+    winner?: string
+    soldAmount?: number
+    currentHighBid?: number
+    createdAt: string
+    updatedAt: string
+    auctionEnd: string
+    status: string
+    name: string
+    year: number
+    genre: string
+    imageUrl: string
+    id: string
+}
