338f44ba32d526d01641327b446f555ddda9ef0f
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-23 12:34:55 +0200

    Create CountdownTimer

--- frontend/web-app/app/auctions/CountdownTimer.tsx
+++ frontend/web-app/app/auctions/CountdownTimer.tsx
@@ -1,7 +1,34 @@
-export default function CountdownTimer() {
+'use client';
+
+import Countdown, { zeroPad } from "react-countdown";
+
+const renderer = ({ days, hours, minutes, seconds, completed }:
+    { days: number, hours: number, minutes: number, seconds: number, completed: boolean }) => {
+    
+    return (
+        <div className={`
+            text-white py-1 px-2 rounded-lg flex justify-center
+            ${completed ? 'bg-red-800' : (days === 0 && hours < 10) 
+                ? 'bg-red-900' : 'bg-amber-900'}
+        `}>
+            {completed ? (
+                <span>Auction finished</span>
+            ) : (
+                <span suppressHydrationWarning={true}>
+                    {days}:{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}</span>
+            )}
+        </div>
+    )
+};
+
+type Props = {
+    auctionEnd: string;
+}
+
+export default function CountdownTimer({auctionEnd}: Props) {
     return (
         <div>
-            CountdownTimer
+            <Countdown date={auctionEnd} renderer={renderer} />
         </div>
     )
 }
