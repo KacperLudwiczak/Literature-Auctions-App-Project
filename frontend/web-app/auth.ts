d7267f581b8a5a0e9caf95e3c00057c4197d0bb3
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-24 10:16:53 +0200

    Update Auth

--- frontend/web-app/auth.ts
+++ frontend/web-app/auth.ts
@@ -13,25 +13,25 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             idToken: true
         } as OIDCConfig<Omit<Profile, 'username'>>),
     ],
-    // callbacks: {
-    //     async authorized({auth}) {
-    //     return !!auth;
-    //     },
-    //     async jwt({token, profile, account}) {
-    //     if (account && account.access_token) {
-    //         token.accessToken = account.access_token
-    //     }
-    //     if (profile) {
-    //         token.username = profile.username
-    //     }
-    //     return token;
-    //     },
-    //     async session({session, token}) {
-    //     if (token) {
-    //         session.user.username = token.username;
-    //         session.accessToken = token.accessToken;
-    //     }
-    //     return session;
-    //     }
-    // }
+    callbacks: {
+        async authorized({auth}) {
+        return !!auth;
+        },
+        async jwt({token, profile, account}) {
+        if (account && account.access_token) {
+            token.accessToken = account.access_token
+        }
+        if (profile) {
+            token.username = profile.username
+        }
+        return token;
+        },
+        async session({session, token}) {
+        if (token) {
+            session.user.username = token.username;
+            session.accessToken = token.accessToken;
+        }
+        return session;
+        }
+    }
 })
