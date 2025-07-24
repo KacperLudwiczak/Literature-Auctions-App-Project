5fbfde35630ed82283c465e2129895c600156572
KacperLudwiczak <casper.ludwiczak@gmail.com>
2025-07-24 10:23:38 +0200

    Add Router to Logo

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,13 +1,21 @@
 'use client';
 
 import { useParamsStore } from "@/app/hooks/useParamsStore";
+import { usePathname, useRouter } from "next/navigation";
 import { FaBookBookmark } from "react-icons/fa6";
 
 export default function Logo() {
+    const router = useRouter();
+    const pathname = usePathname();
     const reset = useParamsStore(state => state.reset);
 
+    function handleReset() {
+        if (pathname !== '/') router.push('/');
+        reset();
+    }
+
     return (
-        <div onClick={reset}  className="cursor-pointer flex items-center gap-2 text-2xl font-semibold text-amber-900">
+        <div onClick={handleReset}  className="cursor-pointer flex items-center gap-2 text-2xl font-semibold text-amber-900">
             <FaBookBookmark size={28} />
             <div>Literature Auctions</div>
         </div>
